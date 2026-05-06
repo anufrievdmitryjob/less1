7663aced3b46176b00eec06282124e1e8dd54a40
User <anufrievdmitryjob@gmail.com>
2026-05-06 20:46:04 +0400

    Test 1

--- test_search.py
+++ test_search.py
@@ -1,29 +1,33 @@
 import pytest
 import time
 from selene import browser, by, have
+from selene.support.conditions import be
 
 
 @pytest.fixture(scope="function", autouse=True)
 def setup_browser():
     browser.config.window_width = 1280
     browser.config.window_height = 720
-    browser.config.timeout = 3
+    browser.config.timeout = 10
 
     print("Браузер настроен")
 
     yield
 
     browser.quit()
-    print("раузер закрыт")
+    print("Браузер закрыт")
 
 def test_search_in_google():
-    browser.open("https://www.google.com/ncr")
-    time.sleep(3)
+    try:
+        browser.open("https://ya.ru")
+        time.sleep(0.5)
 
-    browser.element('[name = "q"').type("petest selene")
-    time.sleep(1)
+        browser.element('[name="text"]').type("pytest selene")
+        time.sleep(1.5)
 
-    browser.element('[name = "q').press_enter()
+        browser.element('[name="text"]').press_enter()
 
-    browser.element(by.partial_text("Selene - GitHub")).should(have.text("Selene - GitHub"))
-    print ("Всё нашлось")
+        browser.element(by.partial_text("pytest-selene")).should(be.visible)
+        print ("Всё нашлось")
+    except Exception as e:
+        print ("Не вышло")
